90cbdd513506550446d8d9e3af5e1ee701e4f8c7
Joe Kutner <jpkutner@gmail.com>
2016-06-01 15:54:20 -0500

    added jpa stuff

--- src/main/java/com/example/EmployeeResource.java
+++ src/main/java/com/example/EmployeeResource.java
@@ -1,7 +1,25 @@
 package com.example;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 /**
- * Created by jkutner on 6/1/16.
+ * @author Ken Finnigan
  */
+@Path("/")
+@ApplicationScoped
 public class EmployeeResource {
+
+  @PersistenceContext
+  EntityManager em;
+
+  @GET
+  @Produces("application/json")
+  public Employee[] get() {
+    return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
+  }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,40 @@
 package com.example;
 
-/**
- * Created by jkutner on 6/1/16.
- */
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.wildfly.swarm.jpa.JPAFraction;
+
 public class Main {
+  public static void main(String[] args) throws Exception {
+    Container container = new Container();
+
+    container.fraction(new DatasourcesFraction()
+        .dataSource("MyDS", (ds) -> {
+          ds.driverName("h2");
+          ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+          ds.userName("sa");
+          ds.password("sa");
+        })
+    );
+
+    // Prevent JPA Fraction from installing it's default datasource fraction
+    container.fraction(new JPAFraction()
+        .inhibitDefaultDatasource()
+        .defaultDatasource("jboss/datasources/MyDS")
+    );
+
+    container.start();
+
+    JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+    deployment.addClasses(Employee.class);
+    deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/persistence.xml", Main.class.getClassLoader()), "classes/META-INF/persistence.xml");
+    deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/load.sql", Main.class.getClassLoader()), "classes/META-INF/load.sql");
+    deployment.addResource(com.example.EmployeeResource.class);
+    deployment.addAllDependencies();
+
+    container.deploy(deployment);
+  }
 }
