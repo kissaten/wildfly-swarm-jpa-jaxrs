16b685c6503db7ef4fe355178fd977d9169a9357
Joe Kutner <jpkutner@gmail.com>
2016-06-02 08:18:50 -0500

    Reworked dependencies

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,6 @@ package com.example;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.datasources.DatasourcesFraction;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 import org.wildfly.swarm.jpa.postgresql.PostgreSQLJPAFraction;
 
@@ -11,19 +10,9 @@ public class Main {
   public static void main(String[] args) throws Exception {
     Container container = new Container();
 
-//    container.fraction(new DatasourcesFraction()
-//        .dataSource("MyDS", (ds) -> {
-//          ds.driverName("h2");
-//          ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-//          ds.userName("sa");
-//          ds.password("sa");
-//        })
-//    );
-
     // Prevent JPA Fraction from installing it's default datasource fraction
     container.fraction(new PostgreSQLJPAFraction()
         .inhibitDefaultDatasource()
-//        .defaultDatasource("jboss/datasources/MyDS")
     );
 
     container.start();
