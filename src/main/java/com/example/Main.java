68618c2e8b92c61ef840f99d7df3d61779ccc4ce
Joe Kutner <jpkutner@gmail.com>
2016-06-02 08:28:24 -0500

    Reworking ds setup

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 import org.wildfly.swarm.jpa.postgresql.PostgreSQLJPAFraction;
 
@@ -10,9 +11,22 @@ public class Main {
   public static void main(String[] args) throws Exception {
     Container container = new Container();
 
+    container.fraction(new DatasourcesFraction()
+        .jdbcDriver("postgresql", (d) -> {
+          d.driverClassName("org.postgresql.Driver");
+        })
+        .dataSource("MyDS", (ds) -> {
+          ds.driverName("postgresql");
+          ds.connectionUrl(System.getenv("JDBC_DATABASE_URL"));
+          ds.userName(System.getenv("JDBC_DATABASE_USERNAME"));
+          ds.password(System.getenv("JDBC_DATABASE_PASSWORD"));
+        })
+    );
+
     // Prevent JPA Fraction from installing it's default datasource fraction
     container.fraction(new PostgreSQLJPAFraction()
         .inhibitDefaultDatasource()
+        .defaultDatasource("jboss/datasources/MyDS")
     );
 
     container.start();
