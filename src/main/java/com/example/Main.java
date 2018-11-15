f98ca6191b26783e5aaf22f08677c1b4b2205a66
Joe Kutner <jpkutner@gmail.com>
2018-11-15 06:37:31 -0600

    Update wildfly main class

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,36 +1,39 @@
 package com.example;
 
-import org.jboss.shrinkwrap.api.ShrinkWrap;
+// import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
-import io.thorntail.container.Container;
-import io.thorntail.datasources.DatasourcesFraction;
-import io.thorntail.jaxrs.JAXRSArchive;
-import io.thorntail.jpa.postgresql.PostgreSQLJPAFraction;
+// import io.thorntail.container.Container;
+// import io.thorntail.datasources.DatasourcesFraction;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    Container container = new Container();
+    // Container container = new Container();
 
-    container.fraction(new DatasourcesFraction()
-        .jdbcDriver("postgresql", (d) -> {
-          d.driverClassName("org.postgresql.Driver");
-          d.driverModuleName("org.postgresql");
-        })
-        .dataSource("MyPU", (ds) -> {
-          ds.driverName("postgresql");
-          ds.connectionUrl(System.getenv("JDBC_DATABASE_URL"));
-          ds.userName(System.getenv("JDBC_DATABASE_USERNAME"));
-          ds.password(System.getenv("JDBC_DATABASE_PASSWORD"));
-        })
-    );
+    // container.fraction(new DatasourcesFraction()
+    //     .jdbcDriver("postgresql", (d) -> {
+    //       d.driverClassName("org.postgresql.Driver");
+    //       d.driverModuleName("org.postgresql");
+    //     })
+    //     .dataSource("MyPU", (ds) -> {
+    //       ds.driverName("postgresql");
+    //       ds.connectionUrl(System.getenv("JDBC_DATABASE_URL"));
+    //       ds.userName(System.getenv("JDBC_DATABASE_USERNAME"));
+    //       ds.password(System.getenv("JDBC_DATABASE_PASSWORD"));
+    //     })
+    // );
 
-    // Prevent JPA Fraction from installing it's default datasource fraction
-    container.fraction(new PostgreSQLJPAFraction()
-        .inhibitDefaultDatasource()
-        .defaultDatasource("jboss/datasources/MyPU")
-    );
+    // // Prevent JPA Fraction from installing it's default datasource fraction
+    // container.fraction(new PostgreSQLJPAFraction()
+    //     .inhibitDefaultDatasource()
+    //     .defaultDatasource("jboss/datasources/MyPU")
+    // );
 
-    container.start();
+    // container.start();
+    Swarm swarm = new Swarm();
 
     JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
     deployment.addClasses(Employee.class);
@@ -39,6 +42,6 @@ public class Main {
     deployment.addResource(com.example.EmployeeResource.class);
     deployment.addAllDependencies();
 
-    container.deploy(deployment);
+    swarm.start().deploy(deployment);
   }
 }
